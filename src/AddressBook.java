787ae5853aeb04ed82b8cb0e3fafdfd4ab908373
rolaelghonimy <rolaelghonimy@cmail.carleton.ca>
2025-09-15 16:52:34 -0400

    ammended add/remove buddy from addy book

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -19,6 +19,9 @@ public class AddressBook {
         buddies.remove(buddy);
     }
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
